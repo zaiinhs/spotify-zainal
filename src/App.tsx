30caa9dde1aea028245f363c22effe45e78b6b50
Zainal Abidin <zainalabidinhsc@gmail.com>
2022-03-19 21:39:38 +0700

    feat: initialize project

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Homework Generasi GIGIH 2.0
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>LOGIN SPOTIFY</button>
       </header>
     </div>
   );
